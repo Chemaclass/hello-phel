5b051bccb57461cd6b44c8cacce1311b12bdf906
Chemaclass <chemaclass@outlook.es>
2022-10-21 19:14:58 +0200

    Use when-not *compile-mode*

--- phel-config.php
+++ phel-config.php
@@ -2,11 +2,8 @@
 
 declare(strict_types=1);
 
-use Gacela\Framework\ClassResolver\Cache\GacelaCache;
-
 return [
     'src-dirs' => ['src'],
     'test-dirs' => ['tests'],
     'vendor-dir' => 'vendor',
-    GacelaCache::KEY_ENABLED => false,
 ];
